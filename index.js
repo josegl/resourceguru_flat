7ee3038cebc9400f37c4e19de85a1981cd6bf08d
Jose G.L <jgl@mailbox.org>
2022-08-08 14:52:08 +0200

    Handle nested null values inside the arrays

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const flat = item => {
   if(item != null && Array.isArray(item)){
-    return item.reduce((res, x) => [...res, ...flat(x)], []);
+    return item.reduce((res, x) => [...res, ...(x === null ? [x] : flat(x))], []); //Here we handle the null values nested in arrays
   }
-  return item != null ? [item] : item;
+  return item != null ? [item] : item; //Here we handle the null and undefined input
 };
 
 module.exports = flat;
